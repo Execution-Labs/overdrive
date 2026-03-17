16c78aff5a64ee936bb25b61c726f83983dfe3d0
Gan <wcgan7@gmail.com>
2026-03-17 16:34:43 +0800

    task(task-3b71f1a971): Comment context formatter with truncation

--- src/agent_orchestrator/comments/__init__.py
+++ src/agent_orchestrator/comments/__init__.py
@@ -1,5 +1,12 @@
 """PR/MR comment data models for platform-agnostic comment integration."""
 
+from .formatter import format_comments_for_prompt
 from .models import CommentPostResult, PRComment, ReviewDecision, ReviewDecisionType
 
-__all__ = ["PRComment", "ReviewDecision", "CommentPostResult", "ReviewDecisionType"]
+__all__ = [
+    "CommentPostResult",
+    "PRComment",
+    "ReviewDecision",
+    "ReviewDecisionType",
+    "format_comments_for_prompt",
+]
